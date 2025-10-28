d44352db5ead51d3c100f4643a8e59f892984f96
Xemophon <maksimtrenev06@gmail.com>
2025-10-28 14:20:23 +0200

    Add files via upload

--- Tests.py
+++ Tests.py
@@ -1,15 +1,20 @@
-print("Test how matrixes and PY co-work")
-matrix_elements = (input("Enter numbers 4 with spaces: ").split())
-size = float(len(matrix_elements) ** 0.5)
-if round(size) != size:
-    print("Error")
-else:
-    size = int(size)
-    matrix = []
-    for i in range(size):
-        row = []
-        for j in range(size):
-            row.append(float(matrix_elements[i * size + j]))
-        matrix.append(row)
-for row in matrix:
-    print(row)
+print("Test how square matrixes and PY co-work")
+x = ""
+while x != "exit":
+    matrix_elements = (input("Enter squarable number of digits with spaces: ").split())
+    size = float(len(matrix_elements) ** 0.5)
+    if round(size) != size:
+        print("Not a square matrix")
+    else:
+        size = int(size)
+        matrix = []
+        for i in range(size):
+            row = []
+            for j in range(size):
+                row.append(float(matrix_elements[i * size + j]))
+            matrix.append(row)
+    for row in matrix:
+        print(row)
+    x = input("Type 'continue' to proceed or 'exit' to quit: ")    
+print("Exited the loop.")
+    
