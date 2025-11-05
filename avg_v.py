ec7a87cafaa253d79c1c9a85f5af4bf9c8e2a45c
Xemophon <maksimtrenev06@gmail.com>
2025-11-05 20:22:25 +0200

    Update avg_v.py

--- avg_v.py
+++ avg_v.py
@@ -8,24 +8,21 @@ if len(lenghts) == len(velocities):
     for h in range(len(lenghts)):
         data_elements.append(lenghts[h])
         data_elements.insert(2*h+1, velocities[h])
-        size = float(len(data_elements) / 2)
-    if size * 2 % 2 != 0:
-        print("Invalid")
-    else:
-        size = int(size)
-        for i in range(size):
-            row = []
-            for j in range(2):
-                row.append(int(data_elements[i*2 + j]))
-            data_matrix.append(row)
-        delta_t = 0
-        delta_s = 0
-        for k in range(size):
-            time = 0
-            time = data_matrix[k][0] / data_matrix[k][1]
-            delta_t += time
-            delta_s += data_matrix[k][0]
-        avg_v = round(delta_s / delta_t)
-        print("Average velocity should be at most " + str(avg_v) + " km/h")
+    size = float(len(data_elements) / 2)
+    size = int(size)
+    for i in range(size):
+        row = []
+        for j in range(2):
+            row.append(int(data_elements[i*2 + j]))
+        data_matrix.append(row)
+    delta_t = 0
+    delta_s = 0
+    for k in range(size):
+        time = 0
+        time = data_matrix[k][0] / data_matrix[k][1]
+        delta_t += time
+        delta_s += data_matrix[k][0]
+    avg_v = round(delta_s / delta_t)
+    print("Average velocity should be at most " + str(avg_v) + " km/h")
 else:
-    print("Not equal data")
+    print("Missing data")
