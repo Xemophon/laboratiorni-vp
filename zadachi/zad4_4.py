9e7cfa3cdf35bc9a11f6760aff0b4dfaed0d63f4
Xemophon <maksimtrenev06@gmail.com>
2025-11-06 20:47:47 +0200

    Add files via upload

---
+++ zadachi/zad4_4.py
@@ -0,0 +1,13 @@
+x = abs(int(input("Number: ")))
+y = 0
+for i in range(x):
+    if x % (i+1) == 1:
+        pass
+    elif x % (i+1) == 0:
+        y += 1
+if y == 2:
+    print("Simple")
+elif y > 2:
+    print("Complex")
+else:
+    print("Neither")
