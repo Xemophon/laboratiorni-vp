793d954e033dc43f94ec25708150435ed1bec74c
Xemophon <maksimtrenev06@gmail.com>
2025-11-12 22:35:26 +0200

    Add files via upload

--- Calculator/Matrixs.py
+++ Calculator/Matrixs.py
@@ -1,10 +1,11 @@
 from kramer import method_of_Kramer
 from determinant import determinant
 from reverse import reverse_matrix
+from gaus_P import gaussian_probability
 
 try:
     print("Starting the calc...")
-    print("Optoions: \n 1.Reverse a Matrix \n 2.Determinant \n 3.Kramer's Method \n 0 to exit")
+    print("Optoions: \n 1.Reverse a Matrix \n 2.Determinant \n 3.Kramer's Method \n 4.Probability \n 0 to exit")
     while True:
         x = int(input("Enter calc number: "))
         if x == 1:
@@ -13,6 +14,8 @@ try:
             determinant()
         elif x == 3:
             method_of_Kramer()
+        elif x == 4:
+            gaussian_probability()
         elif x == 0:
             print("Exiting the program.")
             break
